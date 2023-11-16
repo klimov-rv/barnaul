77a0eeda3aaa94fb5792ada4ee6e918a71341a97
John Doe <johndoe@example.com>
2023-11-16 11:32:32 +0300

    adaptive historical_line + fix zoom

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,78 +1,83 @@
+if ($('#js-historical__line').length > 0) {
 
-var place_preview = (el) => {
-    return {
-        place_group: el.cloneNode(true)
+    var place_preview = (el) => {
+        return {
+            place_group: el.cloneNode(true)
+        }
     }
-}
 
-var place_common = (pointEl, previewEl) => {
-    previewEl.place_group.setAttribute('data-place-id', pointEl.getAttribute('data-point-id'));
-    return {
-        point: pointEl,
-        preview: previewEl,
+    var place_common = (pointEl, previewEl) => {
+        previewEl.place_group.setAttribute('data-place-id', pointEl.getAttribute('data-point-id'));
+        return {
+            point: pointEl,
+            preview: previewEl,
+        }
     }
-}
 
+    function createElemsTooltip(minusTop, minusLeft, elemToCopy) {
+        var copyElem = document.createElement("div");
+        var copyForTippy = copyElem.cloneNode();
+        var innerElem = document.createElement("div");
+        var rect = elemToCopy.getBoundingClientRect();
+        var attrAdd = 'point_' + elemToCopy.getAttribute("place_number");
 
-function createElemsTooltip(minusTop, minusLeft, elemToCopy) {
-    var copyElem = document.createElement("div");
-    var copyForTippy = copyElem.cloneNode();
-    var innerElem = document.createElement("div");
-    var rect = elemToCopy.getBoundingClientRect();
-    var attrAdd = 'point_' + elemToCopy.getAttribute("place_number");
+        innerElem.appendChild(document.createTextNode(elemToCopy.getAttribute("place_number")))
+        copyElem.appendChild(innerElem);
+        copyForTippy.appendChild(copyElem);
 
-    innerElem.appendChild(document.createTextNode(elemToCopy.getAttribute("place_number")))
-    copyElem.appendChild(innerElem);
-    copyForTippy.appendChild(copyElem);
+        copyForTippy.style.width = rect.width + 10 + "px";
+        copyForTippy.style.height = rect.height + 10 + "px";
+        copyForTippy.style.left = rect.left - minusLeft - 5 + "px";
+        copyForTippy.style.top = rect.top - minusTop - 5 + "px";
 
-    console.log(minusTop);
-    console.log(minusLeft);
+        innerElem.classList.add('duplicate_point');
+        copyElem.classList.add('duplicate_point_wrapp');
+        copyElem.setAttribute("place_name", elemToCopy.getAttribute("place_name"));
+        copyForTippy.classList.add('duplicate_point_el', 'historical_line_point', attrAdd);
+        copyForTippy.setAttribute('data-point-id', elemToCopy.getAttribute("place_number"));
 
-    copyForTippy.style.width = rect.width + 10 + "px";
-    copyForTippy.style.height = rect.height + 10 + "px";
-    copyForTippy.style.left = rect.left - minusLeft - 5 + "px";
-    copyForTippy.style.top = rect.top - minusTop - 5 + "px";
-
-    innerElem.classList.add('duplicate_point');
-    copyElem.classList.add('duplicate_point_wrapp');
-    copyElem.setAttribute("place_name", elemToCopy.getAttribute("place_name"));
-    copyForTippy.classList.add('duplicate_point_el', 'historical_line_point', attrAdd);
-    copyForTippy.setAttribute('data-point-id', elemToCopy.getAttribute("place_number"));
-
-    return copyForTippy;
-}
-
-function recalcElemsTooltip(minusTop, minusLeft, originalPoint) {
-    var rect = originalPoint.getBoundingClientRect();
-    // находим дубликат-точку (TODO найти по дата-атрибутам с помощью Vanilla JS)
-    var duplicate_point_el = $('[data-point-id=' + originalPoint.getAttribute("place_number") + ']');
-    console.log(duplicate_point_el);
-    duplicate_point_el.css("left", rect.left - minusLeft - 5);
-    duplicate_point_el.css("top", rect.top - minusTop - 5);
-    // duplicate_point_el.style.left = rect.left - minusLeft - 5 + "px";
-    // duplicate_point_el.style.top = rect.top - minusTop - 5 + "px";
-}
-
-function calIamgeSize(newW = undefined, newH = undefined, imageOriginWidth, imageOriginHeight, svgOrigW, svgOrigH) {
-    if (!newW) {
-        var newW = $(".historical_line_map").width();
-        var newH = $(".historical_line_map").height();
+        return copyForTippy;
     }
-    console.log(newH);
-    var widthOnePercent = imageOriginWidth / 100;
-    var heightOnePercent = imageOriginHeight / 100;
 
-    var newWidthOnePercent = newW / 100;
-    var newHeightOnePercent = newH / 100;
+    function recalcElemsTooltip(minusTop, minusLeft, originalPoint) {
+        var rect = originalPoint.getBoundingClientRect();
 
-    if (newW >= imageOriginWidth) {
+        // находим дубликат-точку (TODO найти по дата-атрибутам с помощью Vanilla JS)
+        var duplicate_point_el = $('[data-point-id=' + originalPoint.getAttribute("place_number") + ']');
 
-        $(".historical_line_svg").attr({
-            width: imageNewWidth,
-            height: imageNewHeight,
-        });
+        duplicate_point_el.css("left", rect.left - minusLeft - 5);
+        duplicate_point_el.css("top", rect.top - minusTop - 5);
+        // duplicate_point_el.style.left = rect.left - minusLeft - 5 + "px";
+        // duplicate_point_el.style.top = rect.top - minusTop - 5 + "px";
+    }
 
-    } else {
+    function mapSizesCheck() {
+        if (window.innerWidth < 640) {
+            return {
+                h: 390,
+                w: 987,
+            }
+        } else if (window.innerWidth < 920) {
+            return {
+                h: 500,
+                w: 1267,
+            }
+        } else {
+            return {
+                h: $(".historical_line_map").height(),
+                w: $(".historical_line_map").width(),
+            }
+        }
+    }
+
+    function calcImageSize(imageOriginWidth, imageOriginHeight, svgOrigW, svgOrigH) {
+        var actualImgSizes = mapSizesCheck();
+
+        var widthOnePercent = imageOriginWidth / 100;
+        var heightOnePercent = imageOriginHeight / 100;
+
+        var newWidthOnePercent = actualImgSizes.w / 100;
+        var newHeightOnePercent = actualImgSizes.h / 100;
 
         var imageCurrentPercentW = svgOrigW / widthOnePercent;
         var imageCurrentPercentH = svgOrigH / heightOnePercent;
@@ -85,114 +90,8 @@ function calIamgeSize(newW = undefined, newH = undefined, imageOriginWidth, imag
             height: imageNewHeight,
         });
     }
-}
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    if ($('#js-historical__line').length > 0) {
-        var main_line = new Vivus('js-historical__line',
-            {
-                start: 'manual',
-                delay: 0,
-                duration: 150,
-                onReady: function (myVivus) {
-
-                    var allPoints = document.querySelectorAll('.svg_line_point');
-                    var map_container = document.querySelector('.svg_animate');
-                    var parentTop = map_container.getBoundingClientRect().top;
-                    var parentleft = map_container.getBoundingClientRect().left;
-
-                    allPoints.forEach((elem) => map_container.appendChild(createElemsTooltip(parentTop, parentleft, elem)));
-
-                    tippy(".duplicate_point_wrapp", {
-                        animation: 'scale-subtle',
-                        placement: 'right',
-                        hideOnClick: false,
-                        interactive: true,
-                        allowHTML: true,
-                        maxWidth: '',
-                        content: (reference) => '<a href="#"><span class="ttip-text">' + reference.getAttribute('place_name') + '</span><svg aria-hidden="true" width="20" height="16"> <use xlink:href="#arrow-right4"></use> </svg></a>',
-                    });
-                }
-            },
-            function (obj) {
-                // obj.el.classList.add('animation-started'); 
-            }
-        );
-
-        gsap.registerPlugin(ScrollTrigger)
-        $(window).on("onbeforeunload", function () {
-            scrollTrigger.clearScrollMemory();
-            window.history.scrollRestoration = "manual";
-        });
-
-
-        // запуск анимации по скроллу
-
-        const historical_places = [];
-        [...document.querySelectorAll('.historical_place__group')].forEach(preview => historical_places.push(place_preview(preview)));
-
-        const historical_place__points = [];
-        [...document.querySelectorAll('.historical_line_point')].forEach((item, pos) => historical_place__points.push(place_common(item, historical_places[pos])));
-
-        historical_place__points.forEach((point) => {
-            const svg_id = point.preview.place_group.getAttribute('data-place-id');
-            if (svg_id === "2") { // место для второй точки закрепляем внутри специального контейнера
-                const map_container = document.querySelector('.zoom_viewBox');
-                map_container.prepend(point.preview.place_group);
-                gsap.to(point.preview.place_group, {
-                    duration: 0.7,
-                    ease: 'expo',
-                    scale: 0,
-                    transformOrigin: "50% 50%"
-                });
-            } // закрепляем места с анимацией
-            else if (point.preview.place_group.viewportElement === null) {
-                const svg_container = document.getElementById('js-historical__line');
-                svg_container.prepend(point.preview.place_group);
-
-                gsap.to(point.preview.place_group, {
-                    duration: 0.7,
-                    ease: 'expo',
-                    scale: 0,
-                    transformOrigin: "50% 50%"
-                });
-            }
-            point.point.addEventListener('mouseenter', () => {
-                point.preview.place_group.classList.add('place_hovered')
-                gsap.killTweensOf(point.preview.place_group);
-                gsap.to(point.preview.place_group, {
-                    opacity: 1,
-                    duration: 0.7,
-                    ease: 'expo',
-                    scale: 1,
-                    transformOrigin: "50% 50%"
-                });
-            });
-            point.point.addEventListener('mouseleave', () => {
-                console.log(point.preview.place_group);
-                gsap.killTweensOf(point.preview.place_group);
-                gsap.to(point.preview.place_group, {
-                    opacity: 0,
-                    duration: 0.7,
-                    ease: 'expo',
-                    scale: 0,
-                    transformOrigin: "50% 50%"
-                });
-            });
-        })
-
-        gsap.to("#js-historical__line", {
-            scrollTrigger: {
-                trigger: ".historical_line_svg",
-                start: "10% bottom",
-                once: true,
-                onToggle: (self) => {
-                    self.trigger.parentNode.classList.add('animation-started');
-                    main_line.play();
-                },
-            }
-        });
+    document.addEventListener('DOMContentLoaded', () => {
 
         // Исходные размеры что бы не потерять точное соотношение
 
@@ -200,13 +99,11 @@ document.addEventListener('DOMContentLoaded', () => {
         var imageH = 762;
         var svgW = 1026;
         var svgH = 762;
-        // TODO переписать undefined
-        calIamgeSize(undefined, undefined, imageW, imageH, svgW, svgH);
+
+        calcImageSize(imageW, imageH, svgW, svgH);
 
         $(window).on("resize", function () {
-            var newW = $(".historical_line_map").width();
-            var newH = $(".historical_line_map").height();
-            calIamgeSize(newW, newH, imageW, imageH, svgW, svgH);
+            calcImageSize(imageW, imageH, svgW, svgH);
 
             var allPoints = document.querySelectorAll('.svg_line_point');
             var map_container = document.querySelector('.svg_animate');
@@ -216,5 +113,129 @@ document.addEventListener('DOMContentLoaded', () => {
             allPoints.forEach((elem) => recalcElemsTooltip(parentTop, parentleft, elem));
 
         });
-    }
-});
+
+
+        if ($('#js-historical__line').length > 0) {
+            var main_line = new Vivus('js-historical__line',
+                {
+                    start: 'manual',
+                    delay: 0,
+                    duration: 150,
+                    onReady: function (myVivus) {
+
+                        var allPoints = document.querySelectorAll('.svg_line_point');
+                        var map_container = document.querySelector('.svg_animate');
+                        var parentTop = map_container.getBoundingClientRect().top;
+                        var parentleft = map_container.getBoundingClientRect().left;
+
+                        allPoints.forEach((elem) => map_container.appendChild(createElemsTooltip(parentTop, parentleft, elem)));
+
+                        tippy(".duplicate_point_wrapp", {
+                            animation: 'scale-subtle',
+                            placement: 'right',
+                            hideOnClick: false,
+                            interactive: true,
+                            allowHTML: true,
+                            maxWidth: '',
+                            content: (reference) => '<a href="#"><span class="ttip-text">' + reference.getAttribute('place_name') + '</span><svg aria-hidden="true" width="20" height="16"> <use xlink:href="#arrow-right4"></use> </svg></a>',
+                        });
+                    }
+                },
+                function (obj) {
+                    // obj.el.classList.add('animation-started'); 
+                }
+            );
+
+            gsap.registerPlugin(ScrollTrigger)
+            $(window).on("onbeforeunload", function () {
+                scrollTrigger.clearScrollMemory();
+                window.history.scrollRestoration = "manual";
+            });
+
+
+            // запуск анимации по скроллу
+
+            const historical_places = [];
+            [...document.querySelectorAll('.historical_place__group')].forEach(preview => historical_places.push(place_preview(preview)));
+
+            const historical_place__points = [];
+            [...document.querySelectorAll('.historical_line_point')].forEach((item, pos) => historical_place__points.push(place_common(item, historical_places[pos])));
+
+            historical_place__points.forEach((point) => {
+                const svg_id = point.preview.place_group.getAttribute('data-place-id');
+
+                // закрепляем места с анимацией
+                if (point.preview.place_group.viewportElement === null) {
+                    // место для второй точки закрепляем внутри специального контейнера 
+                    if (svg_id === "2") {
+                        document.getElementById('zoom_viewBox').appendChild(point.preview.place_group);
+                        gsap.to(point.preview.place_group, {
+                            duration: 0.7,
+                            ease: 'expo',
+                            scale: 0,
+                            transformOrigin: "50% 50%"
+                        });
+                    } else {
+
+                        const svg_container = document.getElementById('js-historical__line');
+                        svg_container.prepend(point.preview.place_group);
+
+                        gsap.to(point.preview.place_group, {
+                            duration: 0.7,
+                            ease: 'expo',
+                            scale: 0,
+                            transformOrigin: "50% 50%"
+                        }); F
+                    }
+
+                }
+                point.point.addEventListener('mouseenter', (e) => {
+                    point.preview.place_group.classList.add('place_hovered');
+                    if (svg_id === "2") {
+                        console.log("mouseenter");
+                        const zoom_container = document.querySelector('.zoom_container');
+                        zoom_container.classList.add('zoom-active');
+                    }
+                    gsap.killTweensOf(point.preview.place_group);
+                    gsap.to(point.preview.place_group, {
+                        opacity: 1,
+                        duration: 0.7,
+                        ease: 'expo',
+                        scale: 1,
+                        transformOrigin: "50% 50%"
+                    });
+                });
+                point.point.addEventListener('mouseleave', (e) => {
+                    point.preview.place_group.classList.remove('place_hovered')
+                    console.log("mouseleave");
+                    console.log(e.fromElement.classList.contains('point_2'))
+                    if (svg_id === "2") {
+                        const zoom_container = document.querySelector('.zoom_container');
+                        zoom_container.classList.remove('zoom-active')
+                    }
+                    gsap.killTweensOf(point.preview.place_group);
+                    gsap.to(point.preview.place_group, {
+                        opacity: 0,
+                        duration: 0.7,
+                        ease: 'expo',
+                        scale: 0,
+                        transformOrigin: "50% 50%"
+                    });
+                });
+            })
+
+            gsap.to("#js-historical__line", {
+                scrollTrigger: {
+                    trigger: ".historical_line_svg",
+                    start: "10% bottom",
+                    once: true,
+                    onToggle: (self) => {
+                        self.trigger.parentNode.classList.add('animation-started');
+                        main_line.play();
+                    },
+                }
+            });
+
+        }
+    });
+}
